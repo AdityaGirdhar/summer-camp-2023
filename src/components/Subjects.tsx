98bedbe4168840adcb11d4320cd83ea0e87b5168
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-05-18 17:09:00 +0530

    added subjects

--- src/components/Subjects.tsx
+++ src/components/Subjects.tsx
@@ -7,12 +7,11 @@ export default function Subjects() {
     <div className='container'>
         <h2>Subjects</h2>
         <div className='cards-grid'>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
-            <Card image={atishiImage} title="SubjectName" content="hfds fdshbff jvk fhdjk vjs vhfsvk fshv fsvnfjksv njk v"/>
+            <Card image={atishiImage} title="Analytical Reasoning" content="Explore the art of logical thinking and problem-solving through puzzles, patterns, and deductive reasoning and critical thinking"/>
+            <Card image={atishiImage} title="Applied Science" content="Explore the practical side of science at our high school summer camp, where students learn how scientific principles are applied in real-world scenarios."/>
+            <Card image={atishiImage} title="Astronomy" content="Discover the vastness of the universe in our high school summer camp, exploring celestial bodies, constellations, and the wonders of astronomy."/>
+            <Card image={atishiImage} title="Life Skills" content="Empowering teens with essential tools for personal growth, resilience, communication and emotional intelligence during a transformative summer experience."/>
+            <Card image={atishiImage} title="Mathematics" content="Explore the fascinating world of numbers, shapes, and patterns in our immersive Mathematics program at the high school summer camp."/>
         </div>
         <hr />
     </div>
