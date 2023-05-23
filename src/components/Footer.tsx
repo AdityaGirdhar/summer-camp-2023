574e362c1830d094e4299c3e7437d093139cf0eb
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-05-23 13:52:14 +0530

    added speakers schedule

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <h5>Open doors to opportunity this summer.</h5>
         <div>
             <p> <span className="gray">Copyright ©</span> Summer Camp IIIT-Delhi</p>
-            <p><span className="gray">Designed and Developed by </span>Aditya Girdhar</p>
+            <p><span className="gray">Designed and Developed by </span> <a target='_blank' href="https://www.linkedin.com/in/adityagirdhar/">Aditya Girdhar</a></p>
         </div>
     </div>
   )
--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import tt from '../assets/tt.svg'
+import ttSpeakers from '../assets/tt-speakers.svg'
 
 export default function TimeTable() {
   return (
     <div className='container'>
         <h2>Time Table</h2>
+        <img src={ttSpeakers} alt="Speakers" style={{marginBottom: '25px'}} />
         <img className='time-table' src={tt} alt="Time-Table" />
         <br />
         <a href='https://drive.google.com/file/d/1nf71QCtUWWiQDFKSuxB67RYAVMywalEO/view?usp=sharing' className='download-pdf' download={true} target='_blank'>Download PDF</a>
